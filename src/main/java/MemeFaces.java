16ee2c736ca6ade12c7410693f9cc980bf37ab5d
kayvan61 <kayvan.mansoor@gmail.com>
2019-11-01 16:21:05 -0500

    removed more logging statements

--- src/main/java/MemeFaces.java
+++ src/main/java/MemeFaces.java
@@ -12,18 +12,13 @@ public class MemeFaces extends BotRunable {
     void doWork() {
         // TODO Auto-generated method stub
         // access super.be;
-        System.out.println("from MemeFaces Task");
         if (super.be.getArguments().get(0).equals("/face")) {
             JSONObject responseBody = new JSONObject();
             responseBody.put("bot_id", BotMain.getBotID());
             responseBody.put("text", CoolFaces.getRandomFace());
 
             super.replyToServer(responseBody.toString());
-        } else {
-	    for(String s: super.be.getArguments()){
-		System.out.println(s);
-	    }
-	}
+        }
     }
 
     @Override
